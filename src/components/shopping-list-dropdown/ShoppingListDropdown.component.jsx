6b9911bc5026413679dc94d17bc999c6297a8048
salihcodev <ahmedsalihdev@gmail.com>
2020-09-25 01:12:28 +0200

    Spread out cart-items on dropdown-cart-list.

--- src/components/shopping-list-dropdown/ShoppingListDropdown.component.jsx
+++ src/components/shopping-list-dropdown/ShoppingListDropdown.component.jsx
@@ -2,14 +2,26 @@
 import React from 'react';
 import './ShoppingListDropdown.sass';
 
+// UTILITIES:
+import { connect } from 'react-redux';
+
 // COMPONENTS:
+import CartItem from '../cart-item/CartItem.component';
 import JokerBtn from '../joker-button/JokerBtn.component';
 
-const ShoppingListDropdown = () => (
+const ShoppingListDropdown = ({ cartItemsList }) => (
   <div className='cart-dropdown'>
-    <div className='cart-items' />
+    <div className='cart-items'>
+      {cartItemsList.map((cartItem) => (
+        <CartItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+    </div>
     <JokerBtn>GO TO CHECKOUT</JokerBtn>
   </div>
 );
 
-export default ShoppingListDropdown;
+const mapStateToProps = ({ cart: { cartItemsList } }) => ({
+  cartItemsList,
+});
+
+export default connect(mapStateToProps)(ShoppingListDropdown);
