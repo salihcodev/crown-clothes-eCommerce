dbf66a4448beee4c32599b3fc7258e59a9a9225b
salihcodev <ahmedsalihdev@gmail.com>
2020-09-27 22:04:31 +0200

    Add redux-persist lib to the project * To store active user data to

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -5,7 +5,19 @@ import { combineReducers } from 'redux';
 import userReducer from './user/userReducer';
 import cartReducer from './cart/cartReducer';
 
-export default combineReducers({
+// PERSIST UTILITIES:
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,20 @@
+// BASIC:
 import rootReducer from './rootReducer';
 import { createStore, applyMiddleware } from 'redux';
+
+// PERSIST UTILITIES:
+import { persistStore } from 'redux-persist';
+
+// MIDDLEWARES:
 import { logger } from 'redux-logger';
 
+// set middleware list
 const middleWares = [logger];
 
+// local store
 const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
-export default store;
+// persistor local store
+const persistor = persistStore(store);
+
+export { store, persistor };
